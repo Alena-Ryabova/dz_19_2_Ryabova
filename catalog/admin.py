07aefc44a45132dd40e821a4a53a10ea338a49cf
Alena_RA <147716748+Alena-Ryabova@users.noreply.github.com>
2024-03-06 12:38:21 +0500

    Задание 5 настройка отображения в административной панели

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ist_display = ('id', 'category_name')
+    list_filter = ('category_name',)
+    search_fields = ('category_name', 'description')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    ist_display = ('id', 'product_name', 'purchase_price', 'category')
+    list_filter = ('category',)
+    search_fields = ('category', 'product_description')
