f299c7aaa7a40832d14b88acc737d8209a9377e5
Alena_RA <147716748+Alena-Ryabova@users.noreply.github.com>
2024-03-21 12:06:31 +0500

    ДЗ 20.2 выполнено

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from catalog.views import index, contact
+from catalog.views import index, contact, product
+
+app_name = 'catalog'
 
 urlpatterns = [
-    path('', index),
-    path('contact/', contact)
+    path('', index, name='index'),
+    path('contact/', contact),
+    path('product/<int:pk>', product, name='product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    product_all = Product.objects.all()
+    context = {
+        'object_list': product_all
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contact(request):
@@ -12,3 +18,11 @@ def contact(request):
         message = request.POST.get('message')
         print(f'Имя - {name}; Телефон - {phone}; Сообщение - {message};')
     return render(request, 'catalog/contact.html')
+
+
+def product(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'object': product_item
+    }
+    return render(request, 'catalog/product.html', context)
