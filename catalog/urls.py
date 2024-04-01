9ce9a04e913ff8cae704d23cdb23db01817d6d5f
Alena_RA <147716748+Alena-Ryabova@users.noreply.github.com>
2024-04-01 12:14:32 +0500

    22.1 доработка - добавлены недостающие миграции в репозиторий - Добавлена настройка удаления продукта - Добавлена проверка запрещенных слов в описание продукта - Добавлена возможность менять активность версии в форму

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from catalog.views import IndexListView, ContactTemplateView, ProductDetailView, ProductCreateView, ProductUpdateView
+from catalog.views import IndexListView, ContactTemplateView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
 app_name = 'catalog'
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
     path('create/', ProductCreateView.as_view(), name='create_product'),
     path('update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.forms import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
-from django.views.generic import ListView, TemplateView, DetailView, UpdateView, CreateView
+from django.views.generic import ListView, TemplateView, DetailView, UpdateView, CreateView, DeleteView
 
 from catalog.forms import ProductForm, VersionForm
 from catalog.models import Product, Version
@@ -54,25 +54,33 @@ class ProductUpdateView(UpdateView):
     form_class = ProductForm
 
     def get_success_url(self):
-        return reverse('catalog:update_product', args=[self.kwargs.get('pk')])
+        return reverse('catalog:product', args=[self.kwargs.get('pk')])
 
     def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(*kwargs)
+        context_data = super().get_context_data(**kwargs)
         VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
         if self.request.method == 'POST':
-            formset = VersionFormset(self.request.POST, instance=self.object)
+            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
         else:
-            formset = VersionFormset(instance=self.object)
+            context_data['formset'] = VersionFormset(instance=self.object)
 
-        context_data['formset'] = formset
         return context_data
 
     def form_valid(self, form):
         context_data = self.get_context_data()
         formset = context_data['formset']
+        print("Debug: Form is valid")
         self.object = form.save()
 
         if formset.is_valid():
             formset.instance = self.object
             formset.save()
+            print("Debug: Formset is valid")
+        else:
+            print("Debug: Formset is not valid:", formset.errors)
         return super().form_valid(form)
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:index')
