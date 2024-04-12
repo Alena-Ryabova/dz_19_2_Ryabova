a0f4e316eec7b558f7e235e68c2b6d052d1a22de
Alena_RA <147716748+Alena-Ryabova@users.noreply.github.com>
2024-04-12 21:52:14 +0500

    22.2 Задание 2 -Верификация почты пользователя через отправленное письмо -Добавлен владелец и ссылка на него в продуктах -добавлена подтверждение почты по ссылке

--- config/settings.py
+++ config/settings.py
@@ -134,8 +134,10 @@ AUTH_USER_MODEL = 'users.User'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'fuckup@oscarbot.ru'
 EMAIL_HOST_PASSWORD = 'AsTSNVv7pun9'
 EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
